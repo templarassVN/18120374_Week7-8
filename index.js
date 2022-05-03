e7ed157fd47c9a3c145233cda913d391ae8c1e0e
templarassVN <53231722+templarassVN@users.noreply.github.com>
2022-05-03 14:07:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,17 +25,17 @@ let processor = {
     computeFrame: function() {
       this.context.drawImage(this.video, 0, 0, this.width, this.height);
       
-      // let frame = this.context.getImageData(0, 0, this.width, this.height);
-      //     let l = frame.data.length / 4;
+      let frame = this.context.getImageData(0, 0, this.width, this.height);
+          let l = frame.data.length / 4;
   
-      // for (let i = 0; i < l; i++) {
-      //   let r = frame.data[i * 4 + 0];
-      //   let g = frame.data[i * 4 + 1];
-      //   let b = frame.data[i * 4 + 2];
-      //   if (g > 100 && r > 100 && b < 43)
-      //     frame.data[i * 4 + 3] = 0;
-      // }
-      // this.ctx2.putImageData(frame, 0, 0);
+      for (let i = 0; i < l; i++) {
+        let r = frame.data[i * 4 + 0];
+        let g = frame.data[i * 4 + 1];
+        let b = frame.data[i * 4 + 2];
+        if (g > 100 && r > 100 && b < 43)
+          frame.data[i * 4 + 3] = 0;
+      }
+      this.context.putImageData(frame, 0, 0);
       return;
     }
   };
